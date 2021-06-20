2cbf1725ae0f11e9ed5b7e68ac40df62fc801c6b
FluxOfPingIntegers <Ryan.M.Schleck@gmail.com>
2021-06-20 11:50:29 -0400

    commit tree dcce9a52cccc69321744d44fae1b5ad29df2e0fe

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,31 @@
 class Song < ActiveRecord::Base
+  belongs_to :artist
+  belongs_to :genre
+  has_many :notes
   # add associations here
+
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by(name: name)
+  end
+
+  def artist_name
+    self.artist ? self.artist.name : nil
+  end
+
+  def genre_id=(num)
+    self.genre = Genre.find_by_id(num)
+  end
+
+  def genre_id
+    self.genre ? self.genre.name : nil
+  end
+
+  def note_contents=(contents)
+    contents.each do |content|
+      self.notes << Note.find_or_create_by(content: content)
+    end
+    self.save
+  end
+
+
 end
